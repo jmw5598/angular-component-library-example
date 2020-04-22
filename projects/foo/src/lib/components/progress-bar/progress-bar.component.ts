6173bd242119a82b5e98219f951c58ffa40bfa40
Jason White <jmw5598@gmail.com>
2020-04-22 19:49:26 -0400

    simplified progress bar

--- projects/foo/src/lib/components/progress-bar/progress-bar.component.ts
+++ projects/foo/src/lib/components/progress-bar/progress-bar.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { ProgressBarFill } from './progress-bar-fill.enum';
 import { ProgressBarService } from './progress-bar.service';
@@ -8,30 +7,20 @@ import { ProgressBarService } from './progress-bar.service';
 @Component({
   selector: 'foo-progress-bar',
   templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.scss']
+  styleUrls: ['./progress-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProgressBarComponent implements OnInit, OnDestroy {
+export class ProgressBarComponent implements OnInit {
   @Input()
   public fill: ProgressBarFill;
 
-  public progressValue: number;
-  private _progressValueSubject: Subject<void>;
+  public progressValue$: Observable<number>;
 
   constructor(private _progressBarService: ProgressBarService) {
     this.fill = ProgressBarFill.PRIMARY;
-    this.progressValue = 0;
-    this._progressValueSubject = new Subject<void>();
   }
 
   ngOnInit(): void {
-    this._progressBarService
-      .onProgressChange()
-      .pipe(takeUntil(this._progressValueSubject))
-      .subscribe(progressValue => this.progressValue = progressValue);
-  }
-
-  ngOnDestroy(): void {
-    this._progressValueSubject.next();
-    this._progressValueSubject.complete();
+    this.progressValue$ = this._progressBarService.onProgressChange()
   }
 }
